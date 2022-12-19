3adcedfee69b8a609e0f0def883d575a6cc3ac7a
haitp <tph.ideas@gmail.com>
2022-12-20 00:35:26 +0900

    Add files via upload

---
+++ baitap1.js
@@ -0,0 +1,6 @@
+var numberOne = prompt("Nhập vào giá trị thứ nhất:");
+var numberTwo = prompt("Nhập vào giá trị thứ hai:");
+console.log("Giá trị NumberOne: ", numberOne);
+console.log("Giá trị NumberTwo: ", numberTwo);
+var sum = parseInt(numberOne) + parseInt(numberTwo)
+console.log("Tổng ", sum);
