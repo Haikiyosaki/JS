556311011e26bc9f4eb8bb749b57a6c486055423
haitp <tph.ideas@gmail.com>
2022-12-20 23:45:18 +0900

    Add files via upload

---
+++ V2_tt1.js
@@ -0,0 +1,36 @@
+// Bài 1
+let i= 10; 
+let f = 20.5;
+let b= true;
+let s = 'Hà Nội';
+console.log("Type of i",typeof(i));
+console.log("Type of f",typeof(f));
+console.log("Type of b",typeof(b));
+console.log("Type of s",typeof(s));
+document.write("Bài 1: <br/>")
+document.write('<br/>')
+document.write('i =' +i)
+document.write('<br/>')
+document.write('f =' +f)
+document.write('<br/>')
+document.write('b =' +b)
+document.write('<br/>')
+document.write('s =' +s)
+document.write('<br/>')
+document.write('<br/>')
+// Bài 2
+document.write("Bài 2: <br/>")
+document.write('<br/>')
+let width = 20, height = 10;
+let area = width*height;
+document.write('Area: '+area)
+// Bài 3
+document.write('<br/>')
+document.write('<br/>')
+document.write("Bài 3: <br/>")
+document.write('<br/>')
+
+let a= parseInt( prompt("Nhập vào giá trị của a: "));
+let c= parseInt(prompt("Nhập vào giá trị của c: "));
+(a%c==0)? alert("a chia hết cho b"): alert("a không chia hết cho c");
+
