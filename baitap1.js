1bc38f889206b9fb15e5eab57cdae01c54dc6036
haitp <tph.ideas@gmail.com>
2022-12-21 00:15:57 +0900

    Add files via upload

--- baitap1.js
+++ baitap1.js
@@ -1,6 +1,28 @@
-var numberOne = prompt("Nhập vào giá trị thứ nhất:");
-var numberTwo = prompt("Nhập vào giá trị thứ hai:");
-console.log("Giá trị NumberOne: ", numberOne);
-console.log("Giá trị NumberTwo: ", numberTwo);
-var sum = parseInt(numberOne) + parseInt(numberTwo)
-console.log("Tổng ", sum);
+// let score_Ly = parseFloat(prompt("Nhập vào điểm môn Lý: ")); 
+// let score_Hoa = parseFloat(prompt("Nhập vào điểm môn Hóa: ")); 
+// let score_Sinh = parseFloat(prompt("Nhập vào điểm môn Sinh: ")); 
+// let sum = score_Hoa + score_Ly + score_Sinh;
+// let avr = sum/3;
+// document.writeln('Tổng điểm 3 môn:  ' +sum)
+// document.write('<br/>')
+// document.writeln('Điểm trung bình: '+avr)
+// document.write('<br/>')
+
+// // Bài 2
+// document.write('Bài 2')
+// document.write('<br/>')
+// let degree_C = parseFloat(prompt("Nhập nhiệt độ  0C:"));
+// let covert_F = degree_C*(9/5) + 32;
+// document.write(' Độ F nhận được:'+covert_F)
+
+// Bài 3
+document.write('Bài 3')
+document.write('<br/>')
+const pi = 3.14;
+let r = parseFloat(prompt("Nhập bán kính hình tròn:"));
+let C = 2*pi*r;
+let S = pi*r*r;
+document.write('Diện tích hình tròn: '+S)
+document.write('<br/>')
+document.write('Chu vi hình tròn: '+C)
+
